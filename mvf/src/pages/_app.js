7f1a24a4d41cfe4a9b05af954166cdc3a836239a
NaisserSteven <zhangc2@illinois.edu>
2023-02-25 17:46:49 -0600

    Fixed gradient

--- mvf/src/pages/_app.js
+++ mvf/src/pages/_app.js
@@ -1,5 +1,5 @@
 import Textbox from '../components/textbox';
-import '@/styles/globals.css'
+// import '@/styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from '../components/navbar';
 import '../styles/textbox.css';
