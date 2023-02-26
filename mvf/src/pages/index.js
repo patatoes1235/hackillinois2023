fe69246e7905bfee3997fec51ced9e3305110633
elddc <emmachen2010@gmail.com>
2023-02-26 12:14:13 -0600

    Remove testing results

--- mvf/src/pages/index.js
+++ mvf/src/pages/index.js
@@ -5,17 +5,17 @@ import Postlist from "@/components/postlist";
 
 function Home() {
 	const [results, setResults] = useState([
-		 {
-		 	title: "Alphabet",
-		 	content: "<p>the <em>quick</em> <span style=\"color: rgb(230, 0, 0);\">brown </span>fox jumped over the <strong>lazy dog</strong></p>"
-		 }, {
-		 	title: "Out of context quote",
-			contact: "123-456-7890",
-		 	content: "<blockquote>What is a volume?</blockquote><p>- Kenny</p>"
-		 }, {
-		 	title: "Kenny's favorite string",
-		 	content: "\tLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br/>\tLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-		 },
+		 //{
+		 //	title: "Alphabet",
+		 //	content: "<p>the <em>quick</em> <span style=\"color: rgb(230, 0, 0);\">brown </span>fox jumped over the <strong>lazy dog</strong></p>"
+		 //}, {
+		 //	title: "Out of context quote",
+			//contact: "123-456-7890",
+		 //	content: "<blockquote>What is a volume?</blockquote><p>- Kenny</p>"
+		 //}, {
+		 //	title: "Kenny's favorite string",
+		 //	content: "\tLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br/>\tLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		 //},
 	]);
 
 	return (
